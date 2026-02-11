d3d537ff29448a9317cd3ee880d626fe91b60d46
Amir <vibelessyoung@gmail.com>
2026-02-11 15:34:36 +0330

    config files

--- src/Layout.jsx
+++
@@ -1,9 +0,0 @@
-function Layout({ children }) {
-  return (
-    <div className="flex flex-col items-center w-full justify-center py-5">
-      {children}
-    </div>
-  );
-}
-
-export default Layout;
