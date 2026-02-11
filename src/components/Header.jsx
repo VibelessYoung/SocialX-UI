c5d04a0b5bf13eec79b7a2837257e0e5aecd45a9
Amir <vibelessyoung@gmail.com>
2026-02-11 21:21:05 +0330

    complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,25 @@ import HeroSection from "./HeroSection";
 
 function Header() {
   return (
-    <>
-      <div className="h-screen w-full bg-blue-950">
-        <Navbar />
-        <HeroSection />
-      </div>
-    </>
+    <div className="relative h-screen w-full bg-blue-950">
+      <Navbar />
+      <HeroSection />
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="absolute bottom-6 left-1/2 -translate-x-1/2 w-8 h-8 text-white animate-bounce"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3"
+        />
+      </svg>
+    </div>
   );
 }
 
