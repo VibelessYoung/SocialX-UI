9a82210ccc62b21230c76e87c7bb71d7e2cdd02b
Amir <vibelessyoung@gmail.com>
2026-02-13 20:21:21 +0330

    start create download secton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Feature from "./components/Feature";
+import Download from "./components/Download";
 
 function App() {
   return (
     <>
       <Header />
       <Feature />
+      <Download />
     </>
   );
 }
