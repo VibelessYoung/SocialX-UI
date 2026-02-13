068c4cb6cd9b3cc82926d08ddfa776c7964ba581
Amir <vibelessyoung@gmail.com>
2026-02-13 16:23:47 +0330

    start create feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Feature from "./components/Feature";
 
 function App() {
   return (
     <>
       <Header />
+      <Feature />
     </>
   );
 }
