b8a1ebfd87f810d044b60acf961b7339e19ec016
Amir <vibelessyoung@gmail.com>
2026-02-13 20:32:49 +0330

    download section created

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Download() {
-  return <div></div>;
+  return (
+    <div className="bg-cyan-600 flex flex-col justify-center items-center h-80">
+      <h1 className="text-2xl text-white font-medium">دانلود اپلیکیشن ما</h1>
+      <div className="w-20 my-4 border-t border-white"></div>
+      <small className="text-white">Lorem ipsum dolor sit amet. </small>
+      <div className="flex gap-3 mt-4">
+        <button className="border border-white p-2 text-white">windows</button>
+        <button className="border border-white p-2 text-white">linux</button>
+        <button className="border border-white p-2 text-white">mac</button>
+      </div>
+    </div>
+  );
 }
 
 export default Download;
